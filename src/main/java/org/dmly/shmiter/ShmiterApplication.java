ca005fd200f0018361c34eb33cb48a85efd7a94d
Dmytro Lysai <dmytro.lysai@netcracker.com>
2023-03-21 19:31:05 +0200

    1

---
+++ src/main/java/org/dmly/shmiter/ShmiterApplication.java
@@ -0,0 +1,13 @@
+package org.dmly.shmiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShmiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShmiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dmly/shmiter/ShmiterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dmly.shmiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShmiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
